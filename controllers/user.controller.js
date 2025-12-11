f793e105446aa44e3d28bac3a2c6416f4a300a41
022Nichapha <66429022@webmail.npru.ac.th>
2025-12-11 18:45:30 +0700

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,26 +5,34 @@ require("dotenv").config();
 const secret = process.env.SECRET;
 
 exports.register = async (req, res) => {
-  const { username, password } = req;
-  if (!username || !password) {
-    res.status(400).send({
-      message: "Please Change username & Password",
-    });
-  }
- const existingUser = await UserModel.ifndOne({ username {);
- if (existingUser) {
-  return  res.status(400).send({
-   message: "This username is already existed",
-  })
- }
+    const {username, password} = req;
+    if(!username || !password) {
+        res.status(400).send({
+            message:"Please Change username & Password"
+        });
+    }
+    const existingUser = await UserModel.findOne({ username });
+    if(existingUser){
+        return res.status(400).send({
+            message: "This username is already existed",
+        })
+    }
 
- try {
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-    const user = await UserModel.create({
-        username
-    })
- }
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        const user = await UserModel.create({
+            username,
+            password:hashedPassword,
+        });
+        res.send(201)({
+            message:"User Register Succesfully",
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.message||"Some Thing Error while registering a new user",
+        });
+    }
 };
 
-exports.login = async (req, res) => {};
+exports.login = async (req,res) => {};
